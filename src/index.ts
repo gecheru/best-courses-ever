f78f8dd1351a36887c59488423fd1e836606786a
Gennady <gecheru@yandex.ru>
2025-09-24 19:45:16 +0300

    https

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import app from './app.ts'
+import https from 'node:https'
+import fs from 'node:fs'
 
 const PORT = 3000
 
-app.listen(PORT, () => {
-  console.log(`App listening on http://localhost:${PORT}`)
+// app.listen(PORT, () => {
+//   console.log(`App listening on http://localhost:${PORT}`)
+// })
+
+const options = {
+  key: fs.readFileSync('.certs/server.key'),
+  cert: fs.readFileSync('.certs/server.cert'),
+}
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`App listening on https://localhost:${PORT}`)
 })
