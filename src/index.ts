eb56799bcfaf375eff46958504dac62950631645
Gennady <gecheru@yandex.ru>
2025-09-24 19:45:46 +0300

    clear

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ import fs from 'node:fs'
 
 const PORT = 3000
 
-// app.listen(PORT, () => {
-//   console.log(`App listening on http://localhost:${PORT}`)
-// })
-
 const options = {
   key: fs.readFileSync('.certs/server.key'),
   cert: fs.readFileSync('.certs/server.cert'),
